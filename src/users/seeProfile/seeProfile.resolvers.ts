70caf61e8c0f6e0ebf8f91040c4a8a8a769ecfd1
jinwood93 <dhehdid1@gmail.com>
2021-10-22 17:41:50 +0900

    Create seeProfile.resolvers.ts

---
+++ src/users/seeProfile/seeProfile.resolvers.ts
@@ -0,0 +1,19 @@
+import client from "../../client"
+import { Resolvers } from "../../types";
+
+const resolvers:Resolvers = {
+  Query:{
+    seeProfile:(_,{username})=>
+    client.user.findUnique({
+      where:{
+        username,
+      },
+      include:{
+        following:true,
+        followers: true,
+      }
+    })
+  }
+}
+
+export default resolvers;
