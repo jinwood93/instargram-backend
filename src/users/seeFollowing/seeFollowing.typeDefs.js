8db60cd949be974581817d70b729273deeba5801
jinwood93 <dhehdid1@gmail.com>
2021-10-22 17:41:45 +0900

    Create seeFollowing.typeDefs.js

---
+++ src/users/seeFollowing/seeFollowing.typeDefs.js
@@ -0,0 +1,15 @@
+import { gql } from "apollo-server-core";
+
+export default gql`
+  
+  type seeFollowingResult{
+    ok : Boolean!
+    error: String
+    following : [User]
+  }
+
+  type Query{
+    seeFollowing(username:String!, lastid:Int) :seeFollowingResult!
+  }
+
+`;
